536dad364682f9bc4f6c0db050c460588cd793c0
gideonthomasd <56291811+gideonthomasd@users.noreply.github.com>
2022-01-28 11:53:55 +0000

    Update 120_get_arcolinux_files.sh

--- 120_get_arcolinux_files.sh
+++ 120_get_arcolinux_files.sh
@@ -7,4 +7,5 @@
 ###############################################################################
 
 yay -S downgrade ttf-font-awesome nerd-fonts-mononoki nerd-fonts-complete 
+yay -S brlaser   #For dcp7055
 yay -S ttf-iosevka-term steam-fonts
