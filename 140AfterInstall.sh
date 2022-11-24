f9064d1ee8cd79dc3f90f571bf043a66b6e23ed9
gideonthomasd <56291811+gideonthomasd@users.noreply.github.com>
2022-11-24 15:16:01 +0000

    Delete 140AfterInstall.sh

--- 140AfterInstall.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-flatpak install flathub com.github.unrud.VideoDownloader
-
-flatpak install flathub io.github.prateekmedia.appimagepool
-
-#sudo snap install brave
-#sudo snap install brave
-##x2?!
